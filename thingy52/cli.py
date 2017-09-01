5cf60e86484c8be256f8ab23fb521598854a4abf
Sebastian Roll <sebastian.roll@webstep.no>
2017-09-01 10:21:26 +0200

    Clean up cli

--- thingy52/cli.py
+++ thingy52/cli.py
@@ -15,10 +15,11 @@ CONTEXT_SETTINGS = dict(
 
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.option('--debug/--no-debug', default=False)
+@click.argument('address')
 @click.pass_context
-def main(ctx, debug):
+def main(ctx, debug, address):
     """Console script for gcloud_cli."""
-    pass
+    ctx.obj['address'] = address
 
 
 @main.command()
@@ -35,27 +36,42 @@ def list_services(ctx):
 
 
 @main.command()
-@click.argument('address')
-def list(address):
+@click.pass_context
+def list_services(ctx):
     """
-    List topics.
+    List services.
     """
+    address = ctx.obj.get("address")
     t = thingy52.Thingy52(address)
     atexit.register(t.disconnect)
 
 
 @main.command()
-@click.argument('address')
+@click.pass_context
+def list_characteristics(ctx):
+    """
+    List characteristics.
+    """
+    address = ctx.obj.get("address")
+    t = thingy52.Thingy52(address)
+    atexit.register(t.disconnect)
+
+
+@main.command()
 @click.argument('feature')
-def listen(address, feature):
+@click.option('--enable', default=True)
+@click.pass_context
+def motion(ctx, feature, enable):
     """
-    List topics.
+    Enable/Disable motion characteristics.
     """
+    address = ctx.obj.get("address")
+    print(address)
+
     t = thingy52.Thingy52(address)
     atexit.register(t.disconnect)
     t.setDelegate(thingy52.ThingyCharDelegate(t.handles))
-    t.motion.toggle_notifications(characteristic=feature)
-    #t.environment.toggle_notifications()
+    t.motion.toggle_notifications(characteristic=feature, enable=enable)
     while True:
         t.waitForNotifications(1.0)
         print("Waiting...")
