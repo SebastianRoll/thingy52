11fd7f8cc2ca17a2e69c0dd92dfb6845da2207ba
Sebastian Roll <sebastian.roll@webstep.no>
2017-09-29 09:32:52 +0200

    Clean up thingyaudio

--- demos/record.py
+++ demos/record.py
@@ -1,21 +1,25 @@
 import atexit
 from time import time
 
-from demos.thingyaudio import RecordingDelegate, RecordingDelegate4, RecordingDelegate5
+from demos.thingyaudio import RecordingDelegate1, RecordingDelegate2, RecordingDelegate3, RecordingDelegate4
 from thingy52.thingy52 import Thingy52
 
-address = "E2:D9:D5:C6:30:26"
-address = "DB:B7:18:34:E3:9E"
+# use your thingy's MAC address printed on the device.
+THINGY_ADDRESS = "E2:D9:D5:C6:30:26"
+RECORD_DURATION_SECONDS = 2
+RECORDING_DELEGATE = RecordingDelegate4
 
-t = Thingy52(address)
+t = Thingy52(THINGY_ADDRESS)
 # Set mic mode to ADPCM
 t.sound.activate_speaker_stream(speaker_mode=1, mic_mode=1)
 
 atexit.register(t.disconnect)
-rec = RecordingDelegate5(t.handles)
+rec = RECORDING_DELEGATE(t.handles)
 t.setDelegate(rec)
 atexit.register(rec.finish)
 t.sound.toggle_notifications(characteristic="microphone", enable=True)
+
+# Record for specified duration. Audio file is written
 now = time()
-while time() - now < 2:
+while time() - now < RECORD_DURATION_SECONDS:
     t.waitForNotifications(1.0)
--- demos/thingyaudio.py
+++ demos/thingyaudio.py
@@ -5,8 +5,6 @@ import wave
 import audioop
 from struct import Struct
 import struct
-import numpy as np
-import scipy.signal as signal
 
 CHUNK = 256
 FORMAT = pyaudio.paInt16
@@ -14,26 +12,14 @@ CHANNELS = 1
 # https://infocenter.nordicsemi.com/index.jsp?topic=%2Fcom.nordic.infocenter.rds%2Fdita%2Frds%2Fdesigns%2Fthingy%2Fintro%2Fkey_features.html
 RATE = 16000
 RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
-
 SILENCE = chr(0)*2
 
-# Intel ADPCM step variation table
-INDEX_TABLE = [-1, -1, -1, -1, 2, 4, 6, 8, -1, -1, -1, -1, 2, 4, 6, 8, ]
-
-# ADPCM step size table
-STEP_SIZE_TABLE = [7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 19, 21, 23, 25, 28, 31, 34, 37, 41, 45, 50, 55, 60, 66, 73, 80,
-                   88, 97, 107, 118, 130, 143, 157, 173, 190, 209,
-                   230, 253, 279, 307, 337, 371, 408, 449, 494, 544, 598, 658, 724, 796, 876, 963, 1060, 1166, 1282,
-                   1411, 1552, 1707, 1878, 2066, 2272, 2499, 2749, 3024, 3327, 3660, 4026, 4428, 4871, 5358,
-                   5894, 6484, 7132, 7845, 8630, 9493, 10442, 11487, 12635, 13899, 15289, 16818, 18500, 20350, 22385,
-                   24623, 27086, 29794, 32767]
-
-
-class RecordingDelegate(DefaultDelegate):
-    def __init__(self, handles):
+# Stream microphone PCM directly to host speaker using pyaudio.PyAudio()
+class RecordingDelegate1(DefaultDelegate):
+    def __init__(self, handles, audio_filename='RecordingDelegate1.wav'):
         DefaultDelegate.__init__(self)
         self.handles = handles
+        self.audio_filename = audio_filename
 
         self.p = pyaudio.PyAudio()
 
@@ -64,7 +50,7 @@ class RecordingDelegate(DefaultDelegate):
         self.stream.close()
         self.p.terminate()
 
-        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        wf = wave.open(self.audio_filename, 'wb')
         wf.setnchannels(CHANNELS)
         wf.setsampwidth(self.p.get_sample_size(FORMAT))
         wf.setframerate(RATE)
@@ -72,13 +58,18 @@ class RecordingDelegate(DefaultDelegate):
         wf.close()
 
 
-class RecordingDelegate3(DefaultDelegate):
-    def __init__(self, handles):
+# Write microphone APCM to file
+# -> open ADPCM file
+# ->  convert ADCPM to linear PCM with audioop.adpcm2lin()
+# -> write PCM to file
+class RecordingDelegate2(DefaultDelegate):
+    def __init__(self, handles, audio_filename='RecordingDelegate3.wav'):
         DefaultDelegate.__init__(self)
         self.handles = handles
         self.p = pyaudio.PyAudio()
         self.frames = []
         self.state = None
+        self.audio_filename = audio_filename
 
         self.adpcmfile = open("recording.adpcm", "wb")
 
@@ -97,40 +88,30 @@ class RecordingDelegate3(DefaultDelegate):
             adpcm = f.read()
         pcm, _ = audioop.adpcm2lin(adpcm, 2, None)
         # data length is 20 bytes, pcm length is 80 bytes
-        self.wavfile = wave.open("out2" + '.wav', 'wb')
+        self.wavfile = wave.open(self.audio_filename, 'wb')
         self.wavfile.setparams((1, 2, 16000, 0, 'NONE', 'NONE'))
         self.wavfile.close()
 
-# b,a=signal.iirdesign(0.03,0.07,5,40)
-# fulldata = np.array([])
-# def callback(in_data, frame_count, time_info, flag):
-#     global audiodatas
-#     print(audiodatas)
-#     if len(audiodatas) == 0:
-#         return (SILENCE, pyaudio.paContinue)
-#     global b, a, fulldata  # global variables for filter coefficients and array
-#     audio_data = np.fromstring(audiodatas, dtype=np.float32)
-#     # do whatever with data, in my case I want to hear my data filtered in realtime
-#     audio_data = signal.filtfilt(b, a, audio_data, padlen=200).astype(np.float32).tostring()
-#     fulldata = np.append(fulldata, audio_data)  # saves filtered data in an array
-#     return (audio_data, pyaudio.paContinue)
-# audiodatas = b'00x'
-class RecordingDelegate4(DefaultDelegate):
-    def __init__(self, handles):
+
+# Like RecordingDelegate1, but using ADPCM-PCM conversion found here:
+# https://github.com/NordicSemiconductor/Nordic-Thingy52-Nodejs/blob/master/examples/microphone.js
+# Custom conversion func `adpcm_decode(data)` and audioop.adpcm2lin() return identical PCM streams.
+class RecordingDelegate3(DefaultDelegate):
+    def __init__(self, handles, audio_filename='RecordingDelegate4.wav'):
         DefaultDelegate.__init__(self)
         self.handles = handles
         self.p = pyaudio.PyAudio()
         self.frames = []
         self.state = None
 
-        self.wavfile = wave.open("out2" + '.wav', 'wb')
+        self.wavfile = wave.open(audio_filename, 'wb')
         self.wavfile.setnchannels(CHANNELS)
         self.wavfile.setsampwidth(self.p.get_sample_size(FORMAT))
         self.wavfile.setframerate(RATE)
 
         self.stream = self.p.open(
             format=pyaudio.paInt16,
-            channels=1, rate=16000, frames_per_buffer=20,
+            channels=1, rate=16000, frames_per_buffer=256,
             output=True) #, stream_callback=callback)#, start=False)
         self.stream.start_stream()
 
@@ -138,12 +119,12 @@ class RecordingDelegate4(DefaultDelegate):
         print("recording")
 
     def handleNotification(self, cHandle, data):
-        # global audiodatas
         # thingy_char = self.handles[cHandle]
         # data = thingy_char.conversion_func(data)
         pcm = adpcm_decode(data)
-        # audiodatas = pcm
         self.stream.write(pcm)
+
+        # Todo: do we need to fill remaining buffer with empty data?
         # free = self.stream.get_write_available()  # How much space is left in the buffer?
         # if free > CHUNK:  # Is there a lot of space in the buffer?
         #     tofill = free - CHUNK
@@ -162,17 +143,17 @@ class RecordingDelegate4(DefaultDelegate):
         self.wavfile.close()
 
 
-class RecordingDelegate5(DefaultDelegate):
-    def __init__(self, handles):
+# Write to wavefile directly, after converting ADPCM->PCM
+# Result: Noise with duration under a second
+class RecordingDelegate4(DefaultDelegate):
+    def __init__(self, handles, audio_filename='RecordingDelegate5.wav'):
         DefaultDelegate.__init__(self)
         self.handles = handles
         self.p = pyaudio.PyAudio()
-        self.wavfile = wave.open("out4" + '.wav', 'wb')
+        self.wavfile = wave.open(audio_filename, 'wb')
         self.wavfile.setnchannels(CHANNELS)
         self.wavfile.setsampwidth(self.p.get_sample_size(FORMAT))
-        self.wavfile.setframerate(int(RATE/60))
-        # self.wavfile.setnframes(256)
-        # self.wavfile.setframerate(RATE)
+        self.wavfile.setframerate(int(RATE))
         self.numframes = 0
         print("* recording")
         self.state = None
@@ -191,7 +172,8 @@ class RecordingDelegate5(DefaultDelegate):
         self.wavfile.close()
 
 
-if __name__ == "__main__":
+# This is just used to test recording of laptop microphone
+def record_laptop_microphone():
     CHUNK = 256
     FORMAT = pyaudio.paInt16
     CHANNELS = 2
@@ -228,6 +210,17 @@ if __name__ == "__main__":
     wf.writeframes(b''.join(frames))
     wf.close()
 
+
+# Intel ADPCM step variation table
+INDEX_TABLE = [-1, -1, -1, -1, 2, 4, 6, 8, -1, -1, -1, -1, 2, 4, 6, 8, ]
+
+# ADPCM step size table
+STEP_SIZE_TABLE = [7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 19, 21, 23, 25, 28, 31, 34, 37, 41, 45, 50, 55, 60, 66, 73, 80,
+                   88, 97, 107, 118, 130, 143, 157, 173, 190, 209,
+                   230, 253, 279, 307, 337, 371, 408, 449, 494, 544, 598, 658, 724, 796, 876, 963, 1060, 1166, 1282,
+                   1411, 1552, 1707, 1878, 2066, 2272, 2499, 2749, 3024, 3327, 3660, 4026, 4428, 4871, 5358,
+                   5894, 6484, 7132, 7845, 8630, 9493, 10442, 11487, 12635, 13899, 15289, 16818, 18500, 20350, 22385,
+                   24623, 27086, 29794, 32767]
 
 def adpcm_decode(adpcm):
     """
